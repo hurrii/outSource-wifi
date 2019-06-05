9bc2fc6528786ea348be85df644736780fa0c27f
Pavel Silenko <hurrii.ftw@gmail.com>
2019-06-05 19:37:04 +0300

    Changes to news pagination's styling

--- src/components/Content/NewsPagination.vue
+++ src/components/Content/NewsPagination.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="news-pagination">
         <div class="btns-wrapper">
-            <div class="btn-switchnews">
+            <div class="chevron">
                 <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M9 15L2 8L9 1" stroke="black" stroke-width="2"/>
                 <path d="M16 8H2" stroke="black" stroke-width="2"/>
                 </svg>
-
-
-                <span>Предыдущая новость</span>
+            </div>
+            <div class="btn-switchnews">
+                Предыдущая новость
             </div>
 
             <div class="vertical-divider"></div>
 
-            <div class="btn-switchnews">
-                <span>Следующая новость</span>
-
+            <div class="btn-switchnews">Следующая новость
+            </div>
+            <div class="chevron">
                 <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M7 1L14 8L7 15" stroke="black" stroke-width="2"/>
                 <path d="M0 8H14" stroke="black" stroke-width="2"/>
@@ -53,31 +53,27 @@ export default {
 
                 .vertical-divider {
                     border-left:1px solid rgba(0, 0, 0, 0.1);
-                    height:80px
+                    height: 80px
                 }
             }
 
             .btn-switchnews {
-                display: inline-block;
-                width: 50%;
+                font-size: 14px;
                 text-align: center;
+                max-width: 40%;
+                flex: 1;
 
-                &:hover {
-                        svg path, span {
-                        color: $pink;
-                        stroke: $pink;
+                @media screen and (min-width: 768px) { font-size: 18px; }
+                @media screen and (min-width: 1024px) { font-size: 20px; }
+            
+            }
+
+
+        svg {
+              @media screen and (max-width: 580px) {
+              width: 10px;
+              height: 10px;
                     }
                 }
-
-
-
-                svg {
-                vertical-align: top;
-
-                    &:first-child { margin-right: 20px }
-                    &:last-child { margin-left: 20px }
-                }
-
-            }
         }
 </style>
